6c025e1f37dd39623eaf85f51b5b294670c0bf7d
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-11 21:30:04 -0300

    feat: create new role

--- iam.tf
+++ iam.tf
@@ -11,6 +11,37 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::525883733118:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:rodrigofontesdev/hello-world-api-iac:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
