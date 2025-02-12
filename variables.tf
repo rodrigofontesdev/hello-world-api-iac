d2b7368954dd7c85155e70b77d6057d08f58cc59
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-02-11 21:55:46 -0300

    fix: remove profile variable

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "profile" {
-  default     = "rodrigofontesdev"
-  type        = string
-  description = "Nome do perfil SSO"
-}
-
 variable "region" {
   default     = "us-east-1"
   type        = string
